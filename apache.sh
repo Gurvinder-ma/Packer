5800a1af4e56702839bdfcd8b7475400426c8a8a
Gurvinder Singh <52526861+Gurvinder-ma@users.noreply.github.com>
2020-06-12 00:38:11 +0530

    Update apache.sh

--- apache.sh
+++ apache.sh
@@ -9,18 +9,3 @@ sudo touch /var/www/html/index.html
 sudo chmod 777 /var/www/html/index.html
 
 echo "<h1>This app is deployed by packer </h1>" > /var/www/html/index.html
-
-
-
-//install & configure Python
-
-//sudo yum -y install gcc openssl-devel bzip2-devel
-//cd /tmp/
-//wget https://www.python.org/ftp/python/3.6.6/Python-3.6.6.tgz
-//tar xzf Python-3.6.6.tgz
-//cd Python-3.6.6
-//./configure --enable-optimizations
-//sudo make altinstall
-//sudo ln -sfn /usr/local/bin/python3.6 /usr/bin/python3.6
-//python3.6 -V
-
